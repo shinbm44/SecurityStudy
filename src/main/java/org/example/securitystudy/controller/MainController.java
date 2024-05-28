3a1d7f8b0ed446e12d0d924982f24e00a4bef3c9
shinbm44 <78016592+shinbm44@users.noreply.github.com>
2024-05-28 10:44:49 +0900

    initial commit

---
+++ src/main/java/org/example/securitystudy/controller/MainController.java
@@ -0,0 +1,15 @@
+package org.example.securitystudy.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping
+    public String mainP() {
+
+        return "main";
+    }
+}
